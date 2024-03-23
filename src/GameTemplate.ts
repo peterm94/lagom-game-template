ffd4453f6a37bdc1f4a84f7c817e0e6f2f2603ab
Peter Mandile <peterm1994@hotmail.com>
2024-03-23 14:58:25 +1100

    added sound manager stuff

--- src/GameTemplate.ts
+++ src/GameTemplate.ts
@@ -1,17 +1,23 @@
-import {AudioAtlas, Game, Log, LogLevel, Scene} from 'lagom-engine';
+import {AudioAtlas, FrameTriggerSystem, Game, Log, LogLevel, Scene, SpriteSheet, TimerSystem} from 'lagom-engine';
 import WebFont from 'webfontloader';
+import muteButtonSpr from "./art/mute_button.png";
+import {SoundManager} from "./util/SoundManager.ts";
 
 class MainScene extends Scene
 {
+    onAdded() {
+        super.onAdded();
 
-}
-
-class DummyScene extends Scene
-{
+        this.addGUIEntity(new SoundManager());
+        this.addGlobalSystem(new TimerSystem());
+        this.addGlobalSystem(new FrameTriggerSystem());
+    }
 }
 
 export class GameTemplate extends Game
 {
+    static GAME_WIDTH = 512;
+    static GAME_HEIGHT = 512;
 
     static muted = false;
     static musicPlaying = false;
@@ -20,22 +26,24 @@ export class GameTemplate extends Game
     constructor()
     {
         super({
-            width: 512,
-            height: 512,
+            width: GameTemplate.GAME_WIDTH,
+            height: GameTemplate.GAME_HEIGHT,
             resolution: 1,
             backgroundColor: 0x200140
         });
 
         // Set the global log level
-        Log.logLevel = LogLevel.WARN;
+        Log.logLevel = LogLevel.DEBUG;
 
+        this.addResource("mute_button", new SpriteSheet(muteButtonSpr, 16, 16));
 
-        this.setScene(new MainScene(this));
+        // Load an empty scene while we async load the resources for the main one
+        this.setScene(new Scene(this));
 
         // Import sounds and set their properties
-        const music = GameTemplate.audioAtlas.load("music", "ADD_ME")
-            .loop(true)
-            .volume(0.3);
+        // const music = GameTemplate.audioAtlas.load("music", "ADD_ME")
+        //     .loop(true)
+        //     .volume(0.3);
 
         // Import fonts
         WebFont.load({
---
+++ src/util/ActionOnPress.ts
@@ -0,0 +1,23 @@
+import {Component, Key, LagomType, System} from "lagom-engine";
+
+export class ActionOnPress extends System<[]> {
+
+    constructor(readonly action: () => void, readonly keys: Key[] = [Key.Space, Key.KeyA, Key.KeyD, Key.KeyW, Key.KeyS, Key.KeyZ, Key.KeyX]) {
+        super();
+    }
+
+    types(): LagomType<Component>[] {
+        return [];
+    }
+
+    update(delta: number): void {
+
+        if (this.scene.game.keyboard.isKeyPressed(...this.keys)) {
+            this.action();
+        }
+    }
+}
+
+// TODO make a title screen using this
+// TODO font loading
+// TODO snippet on how to show debug detect
