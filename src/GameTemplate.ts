498d32c922f11dee67445578eedcd97dec232a7c
Peter Mandile <peterm1994@hotmail.com>
2024-03-24 16:37:56 +1100

    Added scene switching and updated lagom verion

--- src/GameTemplate.ts
+++ src/GameTemplate.ts
@@ -1,7 +1,23 @@
-import {AudioAtlas, FrameTriggerSystem, Game, Log, LogLevel, Scene, SpriteSheet, TimerSystem} from 'lagom-engine';
+import {ActionOnPress, AudioAtlas, Entity, FrameTriggerSystem, Game, Log, LogLevel, Scene, SpriteSheet, TextDisp, TimerSystem} from 'lagom-engine';
 import WebFont from 'webfontloader';
 import muteButtonSpr from "./art/mute_button.png";
 import {SoundManager} from "./util/SoundManager.ts";
+
+class TitleScene extends Scene {
+    onAdded() {
+        super.onAdded();
+
+        this.addGUIEntity(new SoundManager());
+        this.addGlobalSystem(new TimerSystem());
+        this.addGlobalSystem(new FrameTriggerSystem());
+
+        this.addGUIEntity(new Entity("title")).addComponent(new TextDisp(100, 10, "GAME NAME", {fontFamily: "retro", fill: 0xffffff}));
+
+        this.addSystem(new ActionOnPress(() => {
+            this.game.setScene(new MainScene(this.game))
+        }));
+    }
+}
 
 class MainScene extends Scene
 {
@@ -11,6 +27,9 @@ class MainScene extends Scene
         this.addGUIEntity(new SoundManager());
         this.addGlobalSystem(new TimerSystem());
         this.addGlobalSystem(new FrameTriggerSystem());
+
+        this.addGUIEntity(new Entity("main scene")).addComponent(new TextDisp(100, 10, "MAIN SCENE", {fontFamily: "pixeloid", fill: 0xffffff}));
+
     }
 }
 
@@ -33,7 +52,7 @@ export class GameTemplate extends Game
         });
 
         // Set the global log level
-        Log.logLevel = LogLevel.DEBUG;
+        Log.logLevel = LogLevel.WARN;
 
         this.addResource("mute_button", new SpriteSheet(muteButtonSpr, 16, 16));
 
@@ -55,7 +74,7 @@ export class GameTemplate extends Game
         // Wait for all resources to be loaded and then start the main scene.
         this.resourceLoader.loadAll().then(
             () => {
-                this.setScene(new MainScene(this));
+                this.setScene(new TitleScene(this));
             }
         )
 
